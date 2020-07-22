fbfed818d6f394fb3f9f2817b925f92c2655e5c3
Basmatz <59979218+Basmatz@users.noreply.github.com>
2020-07-22 18:46:08 +0200

    update 22.7.

--- Animated_Scatterplot.py
+++ Animated_Scatterplot.py
@@ -1,18 +1,18 @@
 import plotly.express as px
 import pandas as pd
-import matplotlib.pyplot as plt
 
+#Import Data:
 df = pd.read_csv("gapminder_total_fertility.csv")
 df2 = pd.read_excel("gapminder_lifeexpectancy.xlsx")
+df3 = pd.read_excel("gapminder_population.xlsx")
 
-
-#print(df2)
-
+#Transform Columns
 df = df.melt(id_vars="Total fertility rate",
         var_name="year",
         value_name="fertility")
 
 df.rename(columns={"Total fertility rate": "country"}, inplace=True)
+
 
 df2 = df2.melt(id_vars="Life expectancy",
         var_name="year",
@@ -20,15 +20,36 @@ df2 = df2.melt(id_vars="Life expectancy",
 
 df2.rename(columns={"Life expectancy": "country"}, inplace=True)
 
-df["year"] = df["year"].astype(int)
-df2["year"] = df2["year"].astype(int)
 
-df3 = pd.merge(df,df2, how='inner', on=["country","year"])
+df3 = df3.melt(id_vars="Total population",
+        var_name="year",
+        value_name="population")
+
+df3.rename(columns={"Total population": "country"}, inplace=True)
+
+#Change String values to Int
+df["year"] = df["year"].astype(int)
+
+
+df4 = pd.merge(df,df2, how='inner', on=["country","year"])
+df4 = pd.merge(df4,df3, how='inner', on=["country","year"])
+
+
+df4.dropna(inplace=True)
 
 
 #print(df3.where(df3["year"]>1960))
 
-
-fig = px.scatter(df3.loc[(df3["year"] >= 1960)], x="life expectancy", y="fertility", animation_frame="year", animation_group="country", color="country", range_x=[20,100], range_y=[0,10])
+fig = px.scatter(df4.loc[(df4["year"] >= 1960)],
+                 x="life expectancy",
+                 y="fertility",
+                 animation_frame="year",
+                 animation_group="country",
+                 size="population",
+                 color="country",
+                 size_max=60,
+                 hover_name="country",
+                 range_x=[20,100],
+                 range_y=[0,10])
 
 fig.show()
--- test.py
+++ test.py
@@ -1,1 +1,32 @@
-print("test")
+from sklearn.metrics import f1_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import confusion_matrix
+
+
+ypred1 = [0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1]*50
+ytrue1 = [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]*50
+
+ypred = [1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,
+         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]*10
+ytrue = [1,1,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,
+         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]*10
+
+print("\n\nModel 1: \n", confusion_matrix(ypred1, ytrue1), end="\n\n")
+print("Precision: ", precision_score(ypred1, ytrue1))
+print("Accuracy: ", accuracy_score(ypred1, ytrue1))
+print("Recall: ", recall_score(ypred1, ytrue1))
+print("F1: ", f1_score(ypred1, ytrue1))
+print("\n\n")
+print("Model 2: \n", confusion_matrix(ypred, ytrue), end="\n\n")
+print("Precision: ", precision_score(ypred, ytrue))
+print("Accuracy: ", accuracy_score(ypred, ytrue))
+print("Recall: ", recall_score(ypred, ytrue))
+print("F1: ", f1_score(ypred, ytrue))
